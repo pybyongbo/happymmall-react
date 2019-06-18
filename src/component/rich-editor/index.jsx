7f64a2d3c2519f47045d8465a8cdba1ed1be5b58
彭永波 <yongbo.peng@molbase.com>
2019-06-18 18:11:22 +0800

    商品详情页面代码提交

---
+++ src/component/rich-editor/index.jsx
@@ -0,0 +1,51 @@
+import React from  'react';
+import Simditor from 'simditor';
+import 'simditor/styles/simditor.scss';
+
+class RichEditor extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount(){
+        this.loadEditor();
+    }
+
+    componentWillReceiveProps(nextProps){
+        let detailChanged  = this.props.defaultDetail !== nextProps.defaultDetail;
+        if(!detailChanged){
+            return;
+        }
+        this.simditor.setValue(nextProps.defaultDetail);
+    }
+
+    loadEditor(){
+        let element = this.refs['textarea'];
+        this.simditor = new Simditor({
+            textarea:$(element),
+            defaultValue:this.props.placeholder || '请输入文本...',
+            upload:{
+                url:'/manage/product/richtext_img_upload.do',
+                defaultImage:'',
+                fileKey:'upload_file'
+            }
+        })
+        this.bindEditorEvent();
+    }
+
+    bindEditorEvent(){
+        this.simditor.on('valuechanged',(e)=>{
+            this.props.onValueChange(this.simditor.getValue());
+        })
+    }
+
+    render(){
+        return (
+            <div className="rich-editor">
+                <textarea ref="textarea"></textarea>
+            </div>
+        )
+    }
+}
+
+export default RichEditor;
